7a86666168a57394ae1007706326de44478a450f
denliehoo <denliehoo@gmail.com>
2025-07-21 22:55:57 +0800

    chore: Update next config to fix keyv web3 issue

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
-import type { NextConfig } from 'next'
- 
+import type { NextConfig } from 'next';
+
 const nextConfig: NextConfig = {
   output: 'export', // Outputs a Single-Page Application (SPA)
   distDir: 'build', // Changes the build output directory to `build`
-}
- 
-export default nextConfig
+  webpack: (config) => {
+    config.externals.push(
+      '@keyv/redis',
+      '@keyv/mongo',
+      '@keyv/sqlite',
+      '@keyv/postgres',
+      '@keyv/mysql',
+      '@keyv/etcd',
+    );
+    return config;
+  },
+};
+
+export default nextConfig;
