12badc41676075651eb9a3fa85510a4c622bed49
denliehoo <denliehoo@gmail.com>
2025-08-26 23:48:45 +0800

    chore: Update sepolia default asset info

--- src/constants/default-asset-info/sepoliaDefaultAssetInfo.ts
+++ src/constants/default-asset-info/sepoliaDefaultAssetInfo.ts
@@ -31,4 +31,24 @@ export const sepoliaDefaultAssetInfo: IDefaultAssetInfo[] = [
     address: '0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14',
     isDefaultAsset: true,
   },
+  {
+    symbol: 'USDT',
+    name: 'Tether',
+    imgUrl:
+      'https://assets.coingecko.com/coins/images/325/large/Tether-logo.png?1598003707',
+    bal: 0.01,
+    decimals: 18,
+    address: '0xaA8E23Fb1079EA71e0a56F48a2aA51851D8433D0',
+    isDefaultAsset: true,
+  },
+  {
+    symbol: 'LINK',
+    name: 'Chainlink',
+    imgUrl:
+      'https://assets.coingecko.com/coins/images/877/standard/chainlink-new-logo.png?1696502009',
+    bal: 0.01,
+    decimals: 18,
+    address: '0x779877A7B0D9E8603169DdbD7836e478b4624789',
+    isDefaultAsset: true,
+  },
 ];
