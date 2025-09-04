24c826fbb8787d3118665ba2dac53946ab3afa23
denliehoo <denliehoo@gmail.com>
2025-09-04 21:19:48 +0800

    chore: Remove nextjs slug and move into app

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -3,7 +3,7 @@
 import NavBar from '@src/components/layout/NavBar';
 import dynamic from 'next/dynamic';
 
-const App = dynamic(() => import('../../pages/AppPage'), { ssr: false });
+const App = dynamic(() => import('../pages/AppPage'), { ssr: false });
 
 export function ClientOnly() {
   return (
